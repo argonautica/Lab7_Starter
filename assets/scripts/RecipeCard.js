6459e61f93de116d4372a7acf69c98a08683d510
argonautica <sidplusk@yahoo.com>
2022-11-14 23:11:55 -0800

    test

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -1,35 +1,30 @@
 // RecipeCard.js
-
 class RecipeCard extends HTMLElement {
-  // Called once when document.createElement('recipe-card') is called, or
-  // the element is written into the DOM directly as <recipe-card>
-  constructor() {
-    super(); // Inheret everything from HTMLElement
-
-    // Attaches the shadow DOM to this Web Component
-    this.attachShadow({ mode: 'open' });
-
-    // This element will hold our markup once our data is set
-    const article = document.createElement('article');
-
-    // This style element will hold all of the styles for the Web Component
-    const styles = document.createElement('style');
-    // Insert all of the styles in to the <style> element
-    styles.innerHTML = `
+    // Called once when document.createElement('recipe-card') is called, or
+    // the element is written into the DOM directly as <recipe-card>
+    constructor() {
+        super(); // Inheret everything from HTMLElement
+        // Attaches the shadow DOM to this Web Component
+        this.attachShadow({
+            mode: 'open'
+        });
+        // This element will hold our markup once our data is set
+        const article = document.createElement('article');
+        // This style element will hold all of the styles for the Web Component
+        const styles = document.createElement('style');
+        // Insert all of the styles in to the <style> element
+        styles.innerHTML = `
       * {
         font-family: sans-serif;
         margin: 0;
         padding: 0;
       }
-      
       a {
         text-decoration: none;
       }
-
       a:hover {
         text-decoration: underline;
       }
-      
       article {
         align-items: center;
         border: 1px solid rgb(223, 225, 229);
@@ -41,20 +36,17 @@ class RecipeCard extends HTMLElement {
         padding: 0 16px 16px 16px;
         width: 178px;
       }
-
       div.rating {
         align-items: center;
         column-gap: 5px;
         display: flex;
       }
-      
       div.rating > img {
         height: auto;
         display: inline-block;
         object-fit: scale-down;
         width: 78px;
       }
-
       article > img {
         border-top-left-radius: 8px;
         border-top-right-radius: 8px;
@@ -63,18 +55,15 @@ class RecipeCard extends HTMLElement {
         margin-left: -16px;
         width: calc(100% + 32px);
       }
-
       p.ingredients {
         height: 32px;
         line-height: 16px;
         padding-top: 4px;
         overflow: hidden;
       }
-      
       p.organization {
         color: black !important;
       }
-
       p.title {
         display: -webkit-box;
         font-size: 16px;
@@ -84,48 +73,43 @@ class RecipeCard extends HTMLElement {
         -webkit-line-clamp: 2;
         -webkit-box-orient: vertical;
       }
-
       p:not(.title), span, time {
         color: #70757A;
         font-size: 12px;
       }
     `;
-
-    // Append the <style> and <article> elements to the Shadow DOM
-    this.shadowRoot.append(styles, article);
-  }
-
-  /**
-   * Called when the .data property is set on this element.
-   * 
-   * For Example:
-   * let recipeCard = document.createElement('recipe-card'); // Calls constructor()
-   * recipeCard.data = { foo: 'bar' } // Calls set data({ foo: 'bar' })
-   *
-   * @param {Object} data - The data to pass into the <recipe-card>, must be of the
-   *                        following format:
-   *                        {
-   *                          "imgSrc": "string",
-   *                          "imgAlt": "string",
-   *                          "titleLnk": "string",
-   *                          "titleTxt": "string",
-   *                          "organization": "string",
-   *                          "rating": number,
-   *                          "numRatings": number,
-   *                          "lengthTime": "string",
-   *                          "ingredients": "string"
-   *                        }
-   */
-  set data(data) {
-    // If nothing was passed in, return
-    if (!data) return;
-
-    // Select the <article> we added to the Shadow DOM in the constructor
-    const article = this.shadowRoot.querySelector('article');
-
-    // Set the contents of the <article> with the <recipe-card> template and
-    // the data passed in
-    article.innerHTML = `
+        // Append the <style> and <article> elements to the Shadow DOM
+        this.shadowRoot.append(styles, article);
+    }
+    /**
+     * Called when the .data property is set on this element.
+     * 
+     * For Example:
+     * let recipeCard = document.createElement('recipe-card'); // Calls constructor()
+     * recipeCard.data = { foo: 'bar' } // Calls set data({ foo: 'bar' })
+     *
+     * @param {Object} data - The data to pass into the <recipe-card>, must be of the
+     *                        following format:
+     *                        {
+     *                          "imgSrc": "string",
+     *                          "imgAlt": "string",
+     *                          "titleLnk": "string",
+     *                          "titleTxt": "string",
+     *                          "organization": "string",
+     *                          "rating": number,
+     *                          "numRatings": number,
+     *                          "lengthTime": "string",
+     *                          "ingredients": "string"
+     *                        }
+     */
+    set data(data) {
+        // If nothing was passed in, return
+        if (!data) return;
+        // Select the <article> we added to the Shadow DOM in the constructor
+        const article = this.shadowRoot.querySelector('article');
+        // Set the contents of the <article> with the <recipe-card> template and
+        // the data passed in
+        article.innerHTML = `
       <img src="${data.imgSrc}" alt="${data.imgAlt}">
       <p class="title">
         <a href="${data.titleLnk}">${data.titleTxt}</a>
@@ -139,8 +123,7 @@ class RecipeCard extends HTMLElement {
       <time>${data.lengthTime}</time>
       <p class="ingredients">${data.ingredients}</p>
     `;
-  }
+    }
 }
-
 // Define the Class so you can use it as a custom element
 customElements.define('recipe-card', RecipeCard);
